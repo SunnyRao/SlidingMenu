864b247660dd8e5ca97d8b6c55662b174876e332
Sunny <sunnyraocn@gmail.com>
2015-07-09 16:25:53 +0800

    an easy sliding menu

--- src/com/sunnyrao/slidingmenu/SlidingMenu.java
+++ src/com/sunnyrao/slidingmenu/SlidingMenu.java
@@ -1,13 +1,82 @@
 package com.sunnyrao.slidingmenu;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
 import android.util.AttributeSet;
+import android.util.DisplayMetrics;
+import android.util.TypedValue;
+import android.view.MotionEvent;
+import android.view.ViewGroup;
+import android.view.WindowManager;
 import android.widget.HorizontalScrollView;
+import android.widget.LinearLayout;
 
 public class SlidingMenu extends HorizontalScrollView {
 
+	private LinearLayout mWapper;
+	private ViewGroup mMenu;
+	private ViewGroup mContent;
+	private int mScreenWidth;
+	private int mMenuWidth;
+	private int mMenuRightPadding = 50;
+	private boolean once = false;
+
 	public SlidingMenu(Context context, AttributeSet attrs) {
 		super(context, attrs);
+		WindowManager wm = (WindowManager) context
+				.getSystemService(Context.WINDOW_SERVICE);
+		DisplayMetrics outMetrics = new DisplayMetrics();
+		wm.getDefaultDisplay().getMetrics(outMetrics);
+		mScreenWidth = outMetrics.widthPixels;
+		// 把dp转化为px
+		mMenuRightPadding = (int) TypedValue.applyDimension(
+				TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources()
+						.getDisplayMetrics());
+	}
+
+	/**
+	 * 设置子view和自己的宽和高
+	 */
+	@Override
+	protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+		if (!once) {
+			mWapper = (LinearLayout) getChildAt(0);
+			mMenu = (ViewGroup) mWapper.getChildAt(0);
+			mContent = (ViewGroup) mWapper.getChildAt(1);
+			mMenuWidth = mScreenWidth - mMenuRightPadding;
+			mMenu.getLayoutParams().width = mMenuWidth;
+			mContent.getLayoutParams().width = mScreenWidth;
+			once = true;
+		}
+		super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+	}
+
+	/**
+	 * 通过设置偏移量，隐藏menu
+	 */
+	@Override
+	protected void onLayout(boolean changed, int l, int t, int r, int b) {
+		super.onLayout(changed, l, t, r, b);
+		if (changed) {
+			this.scrollTo(mMenuWidth, 0);	
+		}
+	}
+
+	@Override
+	public boolean onTouchEvent(MotionEvent ev) {
+		int action = ev.getAction();
+		switch (action) {
+		case MotionEvent.ACTION_UP:
+			// 隐藏在左边的宽度
+			int scrollX = getScrollX();
+			if (scrollX >= mMenuWidth / 2) {
+				this.smoothScrollTo(mMenuWidth, 0);
+			} else {
+				this.smoothScrollTo(0, 0);
+			}
+			return true;
+		}
+		return super.onTouchEvent(ev);
 	}
 
 }
