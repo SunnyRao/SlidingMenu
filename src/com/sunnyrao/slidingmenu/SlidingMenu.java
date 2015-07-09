ee0298326e6b8e116e8ef19301f143b5c9c1e351
Sunny <sunnyraocn@gmail.com>
2015-07-09 15:06:26 +0800

    create menu layout

---
+++ src/com/sunnyrao/slidingmenu/SlidingMenu.java
@@ -0,0 +1,13 @@
+package com.sunnyrao.slidingmenu;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.HorizontalScrollView;
+
+public class SlidingMenu extends HorizontalScrollView {
+
+	public SlidingMenu(Context context, AttributeSet attrs) {
+		super(context, attrs);
+	}
+
+}
