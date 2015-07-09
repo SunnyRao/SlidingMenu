ace96cca0a9ed7ab28edd9271a656191dec45665
Sunny <sunnyraocn@gmail.com>
2015-07-09 17:12:54 +0800

    add custom attribute

--- src/com/sunnyrao/slidingmenu/SlidingMenu.java
+++ src/com/sunnyrao/slidingmenu/SlidingMenu.java
@@ -1,7 +1,7 @@
 package com.sunnyrao.slidingmenu;
 
-import android.annotation.SuppressLint;
 import android.content.Context;
+import android.content.res.TypedArray;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
 import android.util.TypedValue;
@@ -18,20 +18,47 @@ public class SlidingMenu extends HorizontalScrollView {
 	private ViewGroup mContent;
 	private int mScreenWidth;
 	private int mMenuWidth;
-	private int mMenuRightPadding = 50;
+	private int mMenuRightPadding = 50; // dp
 	private boolean once = false;
 
+	/**
+	 * 未使用自定义属性时调用
+	 */
 	public SlidingMenu(Context context, AttributeSet attrs) {
-		super(context, attrs);
+		this(context, attrs, 0);
+	}
+
+	/**
+	 * 当使用了自定义属性时，调用此构造方法
+	 */
+	public SlidingMenu(Context context, AttributeSet attrs, int defStyle) {
+		super(context, attrs, defStyle);
+		// 获取我们定义的属性
+		TypedArray ta = context.getTheme().obtainStyledAttributes(attrs,
+				R.styleable.SlidingMenu, defStyle, 0);
+		for (int i = 0; i < ta.getIndexCount(); i++) {
+			int attr = ta.getIndex(i);
+			switch (attr) {
+			case R.styleable.SlidingMenu_rightPadding:
+				mMenuRightPadding = ta.getDimensionPixelSize(attr,
+						(int) TypedValue.applyDimension(
+								TypedValue.COMPLEX_UNIT_DIP, 50, context
+										.getResources().getDisplayMetrics()));
+				break;
+			default:
+				break;
+			}
+		}
+		ta.recycle();
 		WindowManager wm = (WindowManager) context
 				.getSystemService(Context.WINDOW_SERVICE);
 		DisplayMetrics outMetrics = new DisplayMetrics();
 		wm.getDefaultDisplay().getMetrics(outMetrics);
 		mScreenWidth = outMetrics.widthPixels;
-		// 把dp转化为px
-		mMenuRightPadding = (int) TypedValue.applyDimension(
-				TypedValue.COMPLEX_UNIT_DIP, 50, context.getResources()
-						.getDisplayMetrics());
+	}
+
+	public SlidingMenu(Context context) {
+		this(context, null);
 	}
 
 	/**
@@ -58,7 +85,7 @@ public class SlidingMenu extends HorizontalScrollView {
 	protected void onLayout(boolean changed, int l, int t, int r, int b) {
 		super.onLayout(changed, l, t, r, b);
 		if (changed) {
-			this.scrollTo(mMenuWidth, 0);	
+			this.scrollTo(mMenuWidth, 0);
 		}
 	}
 
